4ee03aee1ee418a2ecae51eeb17edda169eadbb8
Keisham Thoisana <keishamthoisana@gmail.com>
2023-04-20 18:32:35 +0530

    First Changes added
    Html - added
    Css- added
    Js- just started

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+const inputFieldEl = document.getElementById("input-value")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function(){
+    console.log("click listen")
+})
+
+console.log(inputFieldEl)
