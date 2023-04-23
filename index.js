d655b9f53a34b73497abb06b0e511e1ac7ee3944
Keisham Thoisana <keishamthoisana@gmail.com>
2023-04-23 21:47:20 +0530

    Updated code
    Everything is working fine, accept UI looks

--- index.js
+++ index.js
@@ -1,11 +1,74 @@
+import {initializeApp} from"https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push, onValue, remove} from"https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+const appSettings = {
+    databaseURL: "https://addtolist-22d0b-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const addToListDB = ref(database,"addToList")
 
 
 const inputFieldEl = document.getElementById("input-value")
 const addButtonEl = document.getElementById("add-button")
+const addToListEl = document.getElementById("add-to-list")
 
 addButtonEl.addEventListener("click", function(){
-    console.log("click listen")
+    let inputValue = inputFieldEl.value
+    //console.log(`${inputValue}`)
+    push(addToListDB, inputValue)
+    clearInputFieldEl()
 })
 
-console.log(inputFieldEl)
+onValue(addToListDB,function(snapshot){
+   if(snapshot.exists()){
+        let itemListArray = Object.entries(snapshot.val())
+        //console.log(snapshot.val())
+        clearAddToListEl()
+        for(let i = 0; i<itemListArray.length; i++){
+            let currentItem = itemListArray[i]
+            let currentId = itemListArray[0]
+            let currentValue = itemListArray[1]
+            appendItemToAddToList(currentItem)
+        }
+        //appendItemToAddToList()
+   }
+   else{
+    addToListEl.innerHTML = "List is empty ..."
+   }
+})
+
+function clearAddToListEl(){
+    addToListEl.innerHTML = ""
+}
+
+function clearInputFieldEl(){
+    inputFieldEl.value = ""
+}
+
+function appendItemToAddToList(item){
+    let itemId = item[0]
+    let itemValue = item[1]
+    //addToListEl.innerHTML += `<li>${itemId}</li`
+    let newEl = document.createElement("li")
+    newEl.textContent = itemValue
+    newEl.addEventListener("click", function(){
+        //console.log(itemId)
+        let exactLocationOfItemInDb = ref(database, `addToList/${itemId}`)
+        this.remove(exactLocationOfItemInDb)
+    })
+    addToListEl.append(newEl)
+}
+
+
+
+// newEl.addEventListener("click", function(){
+//     // console.log(itemId)
+//      let exactLocationOfItemInDb = ref(database, `shoppingList/${itemId}`)
+
+//      remove(exactLocationOfItemInDb)
+//  })
+
+//  shoppingListEl.append(newEl)
